c9b23f19c7f7a541f372bb16c573fd6c10e7b1f2
Sambhav987 <sambhavagarwal6@gmail.com>
2021-09-13 14:04:23 +0530

    Added Components

---
+++ components/header.js
@@ -0,0 +1,39 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <header class="bg-gray-600 py-12 px-6" role="banner">
+    <nav class="relative max-w-7xl mx-auto flex items-center justify-between" aria-label="Global">
+    <a href="/" class="text-white font-bold text-2xl hover:text-green-400 lg:text-3xl" title="Abc Def">
+       Sambhav Agarwal.
+    </a>
+    <nav class="inline-block" aria-label="Main">
+       <ul class="flex items-center justify-end space-x-8 text-white font-semibold" role="menubar">
+           <li role="menuitem">
+               <a href="/#" class="text-xl animate-left">Blog</a>
+           </li>
+           <li role="menuitem">
+               <a href="/#" class="text-xl animate-left">Uses</a>
+           </li>
+           <li class="sm:flex" role="menuitem">
+               <a href="https://instagram.com/Sambhav9876" target="_blank" rel="noopener noreferrer" class="hover:text-green-400">
+                   <span class="sr-only">Twitter</span>
+                   <i class="fab fa-instagram fa-2x"></i>
+               </a>
+           </li>
+           <li class="hidden sm:flex" role="menuitem">
+               <a href="https://github.com/sambhav987" rel="noopener noreferrer" class="hover:text-green-400">
+                <i class="fab fa-github fa-2x"></i>
+               </a>
+           </li>
+       </ul>
+    </nav>
+    </nav>
+    </header>
+    `;
+  }
+}
+  customElements.define('header-component', Header);
