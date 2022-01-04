c3d6935b844d22c38a5225ec9b781cd420394590
Sambhav987 <sambhavagarwal6@gmail.com>
2022-01-05 00:19:17 +0530

    test

--- components/header.js
+++ components/header.js
@@ -8,16 +8,16 @@ class Header extends HTMLElement {
     this.innerHTML = `
     <header class="bg-gray-600 py-12 px-6" role="banner">
     <nav class="relative max-w-7xl mx-auto flex items-center justify-between" aria-label="Global">
-    <a href="/" class="text-white font-bold text-2xl hover:text-green-400 lg:text-3xl" title="Sambhav Agarwal">
+    <a href="/" class="text-white font-bold text-2xl hover:text-green-400 lg:text-3xl" title="Abc Def">
        Sambhav Agarwal.
     </a>
     <nav class="inline-block" aria-label="Main">
        <ul class="flex items-center justify-end space-x-8 text-white font-semibold" role="menubar">
            <li role="menuitem">
-               <a href="/tools.html" class="text-xl hover:text-green-400 animate-left">Tools</a>
+               <a href="/tools.html" class="text-xl animate-left">Tools</a>
            </li>
            <li role="menuitem">
-               <a href="/projects.html" class="text-xl hover:text-green-400 animate-left">Projects</a>
+               <a href="/projects.html" class="text-xl animate-left">Projects</a>
            </li>
            <li class="sm:flex" role="menuitem">
                <a href="https://instagram.com/Sambhav9876" target="_blank" rel="noopener noreferrer" class="hover:text-green-400">
@@ -37,4 +37,4 @@ class Header extends HTMLElement {
     `;
   }
 }
-customElements.define('header-component', Header);
+  customElements.define('header-component', Header);
